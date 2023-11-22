3dfba257db54b8afd9438a3e8726e22dc1b19c0c
YadhiraVT <141872532+YadhiraVT@users.noreply.github.com>
2023-11-22 14:39:39 -0600

    Update __init__.py

--- minimal/__init__.py
+++ minimal/__init__.py
@@ -6,3 +6,11 @@ def my_sum(x, y):
 def my_mul(x, y):
     """A function that multiply. """
     return x*y
+def fibonacci_multiplos(n):
+    fibs = []
+    a, b = 0, 1
+    while a < n:
+        if (a%5)==0:
+            fibs.append(a)
+        a, b = b, a+b
+    return fibs
